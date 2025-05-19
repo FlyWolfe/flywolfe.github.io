94a9f58c23609c08280a0cb9784d2f9782fac076
FlyWolfe <mjsherry248@gmail.com>
2025-05-19 00:06:30 -0400

    More particles work

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+var canvas = document.getElementsByTagName('canvas')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
