ecb635bd66da39bd5a4ed6825e84cb930a9c8445
FlyWolfe <mjsherry248@gmail.com>
2025-05-23 04:19:10 -0400

    Totally udpated website

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
-var canvas = document.getElementsByTagName('canvas')
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// sidebar elements
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+
+menu.addEventListener("click", function(){
+  sideBar.classList.remove("close-sidebar");
+  sideBar.classList.add("open-sidebar");
+});
+
+closeIcon.addEventListener("click", function(){
+  sideBar.classList.remove("open-sidebar");
+  sideBar.classList.add("close-sidebar");
+})
